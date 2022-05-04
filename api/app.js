db6779a0015e824b4b69862621d02fee718aefc2
Angela <wonglaichu@gmail.com>
2022-05-04 18:32:13 -0400

    build

--- api/app.js
+++ api/app.js
@@ -20,6 +20,11 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'build'))); 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
